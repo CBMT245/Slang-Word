ae4a8703f9394cc04593b25556dd1c76bf54c8cb
CBMT245 <minhtienst135@gmail.com>
2022-03-29 18:20:44 +0700

    Test IO

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		BufferedWriter write = new Buffere
+		ArrayList<String> list = new ArrayList<String>();
 		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("slang.txt"));
+			String line; 
+			while ((line = reader.readLine()) != null) {
+				list.add(line);
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
 		
-		ArrayList<String> list1 = new ArrayList<String>();
-		list1.add("Ly");
-		list1.add("Minh");
-		list1.add("Tien");
-		System.out.println(list1);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for (int i = 0; i < 10; i++) {
+			String tempt;
+			tempt = list.get(i);
+			System.out.println(tempt);
+		}
+		System.out.println(list.size());
 	}
 
 }
