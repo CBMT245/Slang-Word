f00839e7ac27490c5eda3660b1aa5f6dd83e0aa2
CBMT245 <minhtienst135@gmail.com>
2022-03-29 19:36:07 +0700

    Test HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-
+	static ArrayList<String> list = new ArrayList<String>();
+	static HashMap<String, String> slang = new HashMap<String,String>();
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
+		Scanner scanner = new Scanner(System.in);
+		Input();
+		String data = scanner.nextLine();
+	//	System.out.println(slang);
+		System.out.println(slang.get(data));
+		
+	}
+	
+	public void searchWord(String s) {
+		
+	}
+	
+	public static void Input() {
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("slang.txt"));
 			String line; 
 			while ((line = reader.readLine()) != null) {
 				list.add(line);
+			//	String[] parts = line.split("`");
+			//	slang.put(parts[0], parts[1]);
 			}
 			reader.close();
 		} catch (FileNotFoundException e) {
@@ -25,12 +40,20 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (int i = 0; i < 10; i++) {
+		
+		for (int i = 0; i < list.size(); i++) {
 			String tempt;
 			tempt = list.get(i);
-			System.out.println(tempt);
+			String[] parts = tempt.split("`");
+			if (parts.length == 1) System.out.println(i+1);
+		//	slang.put(parts[0], parts[1]);
+			//System.out.println(parts[1]);
 		}
 		System.out.println(list.size());
+		
 	}
-
+	
 }
+
+
+
