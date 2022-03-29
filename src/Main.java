379a20b5575a38e7d36558a8acc07b7117b50d48
CBMT245 <minhtienst135@gmail.com>
2022-03-29 20:18:50 +0700

    Edit some line in slang.txt and Test some method

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,45 @@ public class Main {
 	static ArrayList<String> list = new ArrayList<String>();
 	static HashMap<String, String> slang = new HashMap<String,String>();
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		
 		Input();
+		Scanner scanner = new Scanner(System.in);
 		String data = scanner.nextLine();
+	//	String data = scanner.nextLine();
 	//	System.out.println(slang);
-		System.out.println(slang.get(data));
+	//	System.out.println(slang.get(data));
+		searchWord(data);
+		list.clear();
+		slang.clear();
+	}
+	//Xong
+	public static void searchWord(String word) {
+		
+		System.out.println(slang.get(word));
+	}
+	//Xong
+	public static void addWord(String key, String def) {
+		slang.put(key, def);
+	}
+	//Xong
+	public static void deleteWord(String key) {
+		boolean check = slang.containsKey(key);
+		if (!check) 
+			System.out.println("Not found the word in dictionary!");
+		else
+			slang.remove(key);
+	}
+	
+	
+	
+	public static void editWord(String key) {
 		
 	}
 	
-	public void searchWord(String s) {
-		
+	public static void resetSlangWord() {
+		for (int i = 0; i < list.size(); i++) {
+			
+		}
 	}
 	
 	public static void Input() {
@@ -29,8 +58,8 @@ public class Main {
 			String line; 
 			while ((line = reader.readLine()) != null) {
 				list.add(line);
-			//	String[] parts = line.split("`");
-			//	slang.put(parts[0], parts[1]);
+				String[] parts = line.split("`");
+				slang.put(parts[0], parts[1]);
 			}
 			reader.close();
 		} catch (FileNotFoundException e) {
@@ -40,7 +69,7 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		/*
 		for (int i = 0; i < list.size(); i++) {
 			String tempt;
 			tempt = list.get(i);
@@ -50,7 +79,12 @@ public class Main {
 			//System.out.println(parts[1]);
 		}
 		System.out.println(list.size());
-		
+	//	System.out.println(list.get(7656));
+		String tempt = list.get(7656);
+		String[] test = tempt.split("`");
+		System.out.println(test[0]);
+		System.out.println(test[1]);
+		*/
 	}
 	
 }
