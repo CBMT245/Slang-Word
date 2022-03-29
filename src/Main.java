a6b1ce848a5c1ec27a756e134fcc449be2bb2cb7
CBMT245 <minhtienst135@gmail.com>
2022-03-29 11:22:41 +0700

    Test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedWriter write = new Buffere
+		
+		
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("Ly");
+		list1.add("Minh");
+		list1.add("Tien");
+		System.out.println(list1);
+	}
+
+}
